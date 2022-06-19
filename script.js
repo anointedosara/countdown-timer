097f35cf02431ed046c2f2ab01a7862d667b0786
anointedosara <101458383+anointedosara@users.noreply.github.com>
2022-06-19 14:51:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // The data/time we want to countdown to
-var countDownDate = new Date("Jun 20, 2022 24:00:00").getTime();
+var countDownDate = new Date("Jun 20, 2022 00:00:00").getTime();
 
 // Run myfunc every second
 var myfunc = setInterval(function() {
